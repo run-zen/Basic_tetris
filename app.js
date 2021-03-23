66b08d75b589a3878e270d4e337030494fdd6571
runzen <55329832+run-zen@users.noreply.github.com>
2021-03-23 17:30:46 +0530

    Added user controls     moveleft() - moves block left     moveRight() - moves block right     rotate() - rotates the block

--- app.js
+++ app.js
@@ -55,35 +55,56 @@ document.addEventListener('DOMContentLoaded', () => {
         [0,1,width,width+1]
     ];
 
+    const colors = ['red','green','blue','brown','yellow','orange','purple']
+
     const thetetromino = [jtetromino,stetromino,itetromino,ztetromino,ltetromino,ttetromino,otetromino];
     let currentPosition = 8;
     let currentRotation = 0;
 
     let random = Math.floor(Math.random() * 7);
+    let randomColor = Math.floor(Math.random() * 7);
+
 
     let current = thetetromino[random][currentRotation];
 
     function draw() {
         current.forEach(index => {
-            squares[currentPosition + index].classList.add('tetromino');
+            squares[currentPosition + index].classList.add(colors[randomColor]);
         })
     }
 
     function undraw() {
         current.forEach(index => {
-            squares[currentPosition + index].classList.remove('tetromino');
+            squares[currentPosition + index].classList.remove(colors[randomColor]);
         })
     }
     
     
     function moveDown() {
+        freeze();
         undraw();
         currentPosition += width;
         draw();
-        freeze();
     }
 
-    let timerId = setInterval(moveDown,600);
+    let timerId = setInterval(moveDown,700);
+
+    function control(e) {
+        if(e.keyCode === 37) {
+            moveLeft();
+        }
+        else if(e.keyCode === 38) {
+            rotate();
+        } 
+        else if(e.keyCode === 39) {
+            moveRight();
+        }
+        else if(e.keyCode === 40) {
+            moveDown();
+        }
+    }
+
+    document.addEventListener('keydown',control);
 
     function freeze() {
         if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
@@ -91,8 +112,49 @@ document.addEventListener('DOMContentLoaded', () => {
 
             // start new tetromino
             random = Math.floor(Math.random() * 7);
+            randomColor = Math.floor(Math.random()*7);
             current = thetetromino[random][currentRotation];
             currentPosition = Math.floor(Math.random() * 17);
+            draw();
+        }
+    }
+
+    function moveLeft() {
+        undraw();
+
+        const isAtRightEdge = current.some(index => (currentPosition + index)%width === 0);
+
+        if(!isAtRightEdge)
+            currentPosition -= 1;
+
+        if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+            currentPosition += 1;
+        }
+        draw();
+    }
+    function moveRight() {
+        undraw();
+
+        const isAtLeftEdge = current.some(index => (currentPosition + index)%width === width-1);
+
+        if(!isAtLeftEdge)
+            currentPosition += 1;
+
+        if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+            currentPosition -= 1;
+        }
+        draw();
+    }
+
+    function rotate() {
+        let temp = thetetromino[random][(currentRotation+1)%4];
+        
+        let isRotatable = temp.some(index => squares[currentPosition + index].classList.contains('taken'));
+
+        if(!isRotatable) {
+            undraw();
+            currentRotation = (currentRotation+1)%4;
+            current = thetetromino[random][currentRotation];
             draw();
         }
     }
